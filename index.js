b73e3d3afd57dd4adc2b4b0a89bd418aa99aa999
mubassyir <amubassyir@gmail.com>
2024-02-06 03:05:36 -0800

    add initial address

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ var lamp2Status = "off";
 var lamp3Status = "off";
 var lamp4Status = "off";
 
-app.get('/',(req,res)=>{
-  res.json({message:"welcome to Express Js"})
-});
+
   /*get status*/
-app.get('/lampStatus', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
       lamp1Status:lamp1Status,
       lamp2Status:lamp2Status,
